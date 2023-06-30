49bd0876595b60c018b756fb5ca64c19601fdcb7
carlos <clementpeter2000@gmail.com git config --global user.name ClementPeter>
2023-06-30 12:43:27 +0100

    “provider_flutter”

--- lib/main.dart
+++ lib/main.dart
@@ -137,7 +137,6 @@ class _MyAppState extends State<MyApp> {
       ],
       child: MaterialApp(
         title: 'Provider_Flutter',
-
         theme: isDark
             ? ThemeData.dark(useMaterial3: true)
             : ThemeData.light(useMaterial3: true),
@@ -200,76 +199,76 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  final String title;
+//   final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
 
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    //Consumer helps to pass data from the change notifier class
-    // in order to trigger an update to the widget we want to update
-    return Consumer<CounterModel>(builder: (context, data, child) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '${data.getCounter}',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: Row(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            FloatingActionButton(
-              onPressed: () {
-                data.incrementCounter();
-              },
-              tooltip: 'Increment',
-              child: const Icon(Icons.add),
-            ),
-            FloatingActionButton(
-              onPressed: () {
-                setState(() {
-                  isDark = false;
-                  //themeData.switchTheme();
-                });
-              },
-              tooltip: 'theme mode',
-              child: isDark
-                  ? const Icon(Icons.dark_mode)
-                  : const Icon(Icons.light_mode),
-            ),
-            FloatingActionButton(
-              onPressed: () {
-                data.decrementCounter();
-              },
-              tooltip: 'Decrement',
-              child: const Icon(Icons.remove),
-            ),
-          ],
-        ),
-      );
-    });
-  }
-}
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     //Consumer helps to pass data from the change notifier class
+//     // in order to trigger an update to the widget we want to update
+//     return Consumer<CounterModel>(builder: (context, data, child) {
+//       return Scaffold(
+//         appBar: AppBar(
+//           title: Text(widget.title),
+//           centerTitle: true,
+//         ),
+//         body: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: <Widget>[
+//               const Text(
+//                 'You have pushed the button this many times:',
+//               ),
+//               Text(
+//                 '${data.getCounter}',
+//                 style: Theme.of(context).textTheme.headlineMedium,
+//               ),
+//             ],
+//           ),
+//         ),
+//         floatingActionButton: Row(
+//           crossAxisAlignment: CrossAxisAlignment.end,
+//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//           children: [
+//             FloatingActionButton(
+//               onPressed: () {
+//                 data.incrementCounter();
+//               },
+//               tooltip: 'Increment',
+//               child: const Icon(Icons.add),
+//             ),
+//             FloatingActionButton(
+//               onPressed: () {
+//                 setState(() {
+//                   isDark = false;
+//                   //themeData.switchTheme();
+//                 });
+//               },
+//               tooltip: 'theme mode',
+//               child: isDark
+//                   ? const Icon(Icons.dark_mode)
+//                   : const Icon(Icons.light_mode),
+//             ),
+//             FloatingActionButton(
+//               onPressed: () {
+//                 data.decrementCounter();
+//               },
+//               tooltip: 'Decrement',
+//               child: const Icon(Icons.remove),
+//             ),
+//           ],
+//         ),
+//       );
+//     });
+//   }
+// }
 
 //
 //
